0ea3a6959569566617629b899face0548fbc158d
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-06 16:48:18 +0530

    add other codes

--- App_to_Bot_communication/App_to_Bot_communication.ino
+++ App_to_Bot_communication/App_to_Bot_communication.ino
@@ -36,21 +36,42 @@ public:
     void HomePage(int* results) {
         if (results[0]) { Serial.print("TimeStamp: "); Serial.println(results[0]); } else {Serial.println("Timestamp Error");}
 
-        if (results[1] == 0) { Serial.print("MessageID 0: "); Serial.println(results[1]);
-
+        if (results[1] == 0) { Serial.print("MessageID 0: Having First Block code "); Serial.println(results[1]);
             if (results[2]) { Serial.print("Control Mode: "); Serial.println(results[2]); }
-
             if (results[3] == 1) {Serial.println("Tool Mode function activated!");} else {Serial.print("Path Mode: "); Serial.println(results[3]);}
-
             if (results[4]) { Serial.print("Lights: "); Serial.println(results[4]); }
-
             if (results[5]) { Serial.print("Emergency: "); Serial.println(results[5]); }
-
             if (results[6]) { Serial.print("Tool Type: "); Serial.println(results[6]); }
 
-        } else if (results[1] == 1) { Serial.print("MessageID 1: Came to Second Block! "); Serial.println(results[1]);  
+        } else if (results[1] == 1) { Serial.print("MessageID 1: Having Second Block code! "); Serial.println(results[1]);  // To enter into this selection can we add code to satisfy the Tool mode activated .?
         
         // Your new code is here 
+            if (results[2]) { 
+                if (results[2] == 1 ){ Serial.println("Selected Tool is Ferro Blade");}
+                else if (results[2] == 2 ){ Serial.println("Selected Tool is Tyne");}
+                else if (results[2] == 3 ){ Serial.println("Selected Tool is Rotavator");}
+                else if (results[2] == 4 ){ Serial.println("Selected Tool is Boom Sprayer");}
+                else if (results[2] == 5 ){ Serial.println("Selected Tool is Mist Blower");}
+                else if (results[2] == 6 ){ Serial.println("Selected Tool is Brush Cutter");}
+                else if (results[2] == 7 ){ Serial.println("Selected Tool is Trailer");}
+                else { Serial.println("Tool selection out of the BOX");}}
+            
+            if (results[3]) {
+              if (results[3] == 1 ){ Serial.println("Selected crop is Chilli");}
+              else if (results[3] == 2 ){ Serial.println("Selected crop is Cotton");}
+              else if (results[3] == 3 ){ Serial.println("Selected crop is Tobacco");}
+              else if (results[3] == 4 ){ Serial.println("Selected crop is Sugarcane");}
+              else { Serial.println("Selected crop is Other than the support");}}
+
+            if (results[4]) {
+              if (results[4] == 1 ){ Serial.println("Selected crop Age is 1 Month");}
+              else if (results[4] == 2 ){ Serial.println("Selected crop Age is 2 Months");}
+              else if (results[4] == 2 ){ Serial.println("Selected crop Age is 3 Months");}}
+
+            if (results[5]) {
+              if (results[5] == 1 ){ Serial.println("Selected Row Spacing is 18 Inches");}
+              else if (results[5] == 2 ){ Serial.println("Selected Row Spacing is 20 Inches");}
+              else if (results[5] == 3 ){ Serial.println("Selected Row Spacing is 23 Inches");}}
         
         }
     }
