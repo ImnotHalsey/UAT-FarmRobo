4d601f217623c3228b8d64de2b41cc23f4cf367c
ImnotHalsey <147918628+ImnotHalsey@users.noreply.github.com>
2023-12-06 15:37:36 +0530

    stable
    .

--- App_to_Bot_communication/App_to_Bot_communication.ino
+++ App_to_Bot_communication/App_to_Bot_communication.ino
@@ -1,55 +1,70 @@
+// Bot To APP --> Mode 50-100ms communication
 #include <SoftwareSerial.h>
-
 SoftwareSerial mySerial(10, 11);
-
-// App to bot on 5-100ms
-class infoDataProcessor {
-public:
-  long get_timestamp() { return 0; }
-  int get_Message_ID() { return 0; }
-  int get_Control_mode() { return random(0, 3); }
-  int get_path_mode() { return random(0, 1); }
-  int get_lights() { return random(0, 2); }
-  int get_Emergency() { return random(0, 2); }
-  int get_tool_type() { return random(0, 6); }};
-
-void Option_Processor() {
-  infoDataProcessor dataProcessor;  // Create an instance of the infoDataProcessor class
-
-  if (dataProcessor.get_path_mode() == 0) {
-    Serial.println("In the Tool mode");
-
-    if (dataProcessor.get_tool_type() == 0) { // Sub Code
-      Serial.println("Tool type is Furrow Blade");
-      
-    } else if (dataProcessor.get_tool_type() == 1) {
-      Serial.println("Tool type is Tyne");
-    } else if (dataProcessor.get_tool_type() == 2) {
-      Serial.println("Tool type is Rotovator");
-    } else if (dataProcessor.get_tool_type() == 3) {
-      Serial.println("Tool type is Boom Sprayer");
-    } else if (dataProcessor.get_tool_type() == 4) {
-      Serial.println("Tool type is Mist Blower");
-    } else if (dataProcessor.get_tool_type() == 5) {
-      Serial.println("Tool type is Brushcutter");
-    } else if (dataProcessor.get_tool_type() == 6) {
-      Serial.println("Tool type is Trailer");
-    } else {Serial.println("Tool selection out of the box");}
-
-
-  } else if (dataProcessor.get_path_mode() == 1) {
-    Serial.println("In the Road mode");
-  } else {
-    Serial.println("Out of Control");
-  }
-}
-
+int t= 0;
 void setup() {
-  Serial.begin(9600);
-  mySerial.begin(9600);
+    Serial.begin(9600);
+    mySerial.begin(9600);
 }
+
+// Class for all communication processing ...
+class SensorDataProcessor {
+public:
+    int get_timestamp() { return 1; }
+    int get_Message_ID() { return random(0, 2); }
+    int get_Control_mode() { return random(0, 3); }
+    int get_path_mode() { return random(0, 2); }
+    int get_lights() { return random(0, 2); }
+    int get_Emergency() { return random(0, 2); }
+    int get_tool_type() { return random(0, 7); }
+
+    void ArrayMaker(int* results) {
+        // Store results in the array
+        results[0] = get_timestamp();
+        results[1] = get_Message_ID();
+        results[2] = get_Control_mode();
+        results[3] = get_path_mode();
+        results[4] = get_lights();
+        results[5] = get_Emergency();
+        results[6] = get_tool_type();
+    }
+};
+
+
+class CommunicationProcessor {
+public:
+    void HomePage(int* results) {
+        if (results[0]) { Serial.print("TimeStamp: "); Serial.println(results[0]); } else {Serial.println("Timestamp Error");}
+
+        if (results[1] == 0) { Serial.print("MessageID 0: "); Serial.println(results[1]);
+
+            if (results[2]) { Serial.print("Control Mode: "); Serial.println(results[2]); }
+
+            if (results[3] == 1) {Serial.println("Tool Mode function activated!");} else {Serial.print("Path Mode: "); Serial.println(results[3]);}
+
+            if (results[4]) { Serial.print("Lights: "); Serial.println(results[4]); }
+
+            if (results[5]) { Serial.print("Emergency: "); Serial.println(results[5]); }
+
+            if (results[6]) { Serial.print("Tool Type: "); Serial.println(results[6]); }
+
+        } else if (results[1] == 1) { Serial.print("MessageID 1: Came to Second Block! "); Serial.println(results[1]);  
+        
+        // Your new code is here 
+        
+        }
+    }
+};
+
+
 
 void loop() {
-  Option_Processor();
-  delay(1000);
+    SensorDataProcessor dataProcessor;
+    CommunicationProcessor communicationProcessor;
+    
+    int results[7];
+    dataProcessor.ArrayMaker(results);
+    communicationProcessor.HomePage(results);
+    
+    delay(2000);
 }
